e7fef8ce3875b5f70e93fb62aa9ab7c7b1967c79
Rede Alexander <redealexander9@gmail.com>
2026-03-22 15:55:28 -0500

    Fixed issue causing images to not display on GitHub pages

--- js/fruits.js
+++ js/fruits.js
@@ -6,32 +6,32 @@ export const fruits = [
     },
     {
         name: "Red Bell Pepper",
-        image: "../images/redBellPepper.jpg",
+        image: "images/redBellPepper.jpg",
         barcode: "000000004688"
     },
     {
         name: "Red Mango",
-        image: "../images/redMango.jpg",
+        image: "images/redMango.jpg",
         barcode: "000000004959"
     },
     {
         name: "Lime",
-        image: "../images/lime.jpg",
+        image: "images/lime.jpg",
         barcode: "0000000040488"
     },
     {
         name: "Lemon",
-        image: "../images/lemon.jpg",
+        image: "images/lemon.jpg",
         barcode: "000000004033"
     },
     {
         name: "Orange",
-        image: "../images/orange.jpg",
+        image: "images/orange.jpg",
         barcode: "000000003107"
     },
     {
         name: "Avocado",
-        image: "../images/avocado.jpg",
+        image: "images/avocado.jpg",
         barcode: "000000004046"
     }
 ]
