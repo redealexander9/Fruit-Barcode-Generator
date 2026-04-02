a48a2c30e720100732e281d0ac6e1d49e94c5010
Rede Alexander <redealexander9@gmail.com>
2026-04-01 20:06:28 -0500

    Added yellow mango and Great Value Devil's food cake

--- js/fruits.js
+++ js/fruits.js
@@ -73,5 +73,15 @@ export const fruits = [
         name: "Garlic Bulb",
         image: "images/garlic.jpg",
         barcode: "4611"
+    },
+    {
+        name: "Devil's Food Cake Mix",
+        image: "images/devilsFood.jpg",
+        barcode: "007874230881"
+    },
+    {
+        name: "Yellow Mango",
+        image: "images/yellowMango.jpg",
+        barcode: "4312"
     }
 ]
